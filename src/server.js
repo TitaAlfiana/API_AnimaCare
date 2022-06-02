5489d5a0ddae0b44def3dacfeddd43c983aa5863
TitaAlfiana <82716874+TitaAlfiana@users.noreply.github.com>
2022-06-02 16:33:11 +0700

    Update server.js

--- src/server.js
+++ src/server.js
@@ -15,7 +15,7 @@ Database.once('open', () => console.log('Database Connected'))
 
 server.use(cors())
 server.use(express.json())
-server.use('/', router)
+server.use('/forum', router)
 
 const port = process.env.PORT || 5000
 server.listen(PORT, () => console.log(`Server berjalan pada port : ${port}`))
